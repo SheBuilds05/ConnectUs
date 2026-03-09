be15c898568e10ebc7885264a49ddf1873828cb5
Khensani Manganye <66848353+khensani1@users.noreply.github.com>
2026-03-09 19:56:44 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,17 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    watch: false,
+  },
 })
