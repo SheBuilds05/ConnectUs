43048e3e3ff4cc82b96a47e22fdf715756c5d608
Khensani Manganye <66848353+khensani1@users.noreply.github.com>
2026-03-09 21:49:23 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,12 +3,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
+  // This tells Vite/Vitest how to handle TypeScript + JSX
+  esbuild: {
+    loader: 'tsx',
+    include: /src\/.*\.[tj]sx?$/,
+    exclude: [],
+  },
   test: {
     globals: true,
     environment: 'jsdom',
