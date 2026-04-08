cf361d5306b34c29e778ff6b3ef7145fd65bf929
Deolyn East <deolyneast@gmail.com>
2026-04-08 13:57:24 +0200

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,7 +3,7 @@ import cors from 'cors';
 import http from 'http';
 import { Server } from 'socket.io';
 import authRoutes from './routes/authRoutes';
-import runnerRoutes from '../src/routes/runnerRoutes'; 
+import runnerRoutes from './routes/runnerRoutes'; 
 import userRoutes from './routes/userRoutes';
 import adminRoutes from './routes/adminRoutes';
 import bookingRoutes from './routes/bookingRoutes';
